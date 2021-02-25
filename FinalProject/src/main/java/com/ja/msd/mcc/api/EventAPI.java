599b9792ad67e24ffaeeef6d4e5e0d9abc607bc5
user <user@this.com>
2021-02-25 08:22:36 -0500

    Added event api

--- FinalProject/src/main/java/com/ja/msd/mcc/api/EventAPI.java
+++ FinalProject/src/main/java/com/ja/msd/mcc/api/EventAPI.java
@@ -1,5 +1,49 @@
 package com.ja.msd.mcc.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.ja.msd.mcc.domain.Customer;
+import com.ja.msd.mcc.domain.Event;
+
 public class EventAPI {
 
+		List<Event> eventList = new ArrayList<Event>();
+		
+		public EventAPI()
+		{
+			Event e1 = new Event(1L, "Event1", "The Bean", "Free");
+			Event e2 = new Event(2L, "Event2", "Beanzz", "Free");
+			eventList.add(e1);
+			eventList.add(e2);
+		}
+		
+		@GetMapping
+		public List<Event> getAll() {
+
+			return eventList;
+		}
+
+		public List<Event> getListOfEvents() {
+			return this.eventList;
+		}
+		
+		public void setListOfEvents(List<Event> listOfEvents) {
+			this.eventList = listOfEvents;
+		}
+		@GetMapping("/{eventId}")
+		public Event getEvent(@PathVariable("eventId") long id) {
+			Event response = null;
+			for (int i = 0; i < eventList.size(); i++) {
+				if (eventList.get(i).getId() == id) {
+					response = eventList.get(i);
+				}
+			}
+			return response;
+		}
+		
+		
 }
--- FinalProject/src/main/java/com/ja/msd/mcc/domain/Event.java
+++ FinalProject/src/main/java/com/ja/msd/mcc/domain/Event.java
@@ -1,5 +1,59 @@
 package com.ja.msd.mcc.domain;
 
-public class Event {
 
+
+public class Event {
+	private long id; 
+	private String code;
+	private String title;
+	private String description;
+		
+	public Event(Long id, String code, String title, String description)
+	{
+		super();
+		this.id = id;
+		this.code = code;
+		this.title = title;
+		this.description = description;
+	}
+	
+	public Long getId()
+	{
+		return id;
+	}
+	
+	public void setID(long id)
+	{
+		this.id = id;
+	}
+	
+	public Long getCode()
+	{
+		return id;
+	}
+	
+	public void setCode(String code)
+	{
+		this.code = code;
+	}
+	
+	public String getTitle()
+	{
+		return title;
+	}
+	
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+	
+	public String getDescription()
+	{
+		return description;
+	}
+	
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
 }
