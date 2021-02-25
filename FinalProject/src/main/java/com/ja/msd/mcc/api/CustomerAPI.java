4e4fc21356212439a7f946d424dd8be49f6ceb71
user <user@this.com>
2021-02-25 16:22:10 -0500

    changes to CustomerAPI

--- FinalProject/src/main/java/com/ja/msd/mcc/api/CustomerAPI.java
+++ FinalProject/src/main/java/com/ja/msd/mcc/api/CustomerAPI.java
@@ -1,54 +1,147 @@
 package com.ja.msd.mcc.api;
 
 
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.ja.msd.mcc.domain.Customer;
+import com.ja.msd.mcc.repository.CustomersRepository;
+
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerAPI {
 
-	ArrayList<Customer> customerList = new ArrayList<Customer>();
 
-	public CustomerAPI() {
-		Customer c1 = new Customer(1, "Anthony", "pass", "anthony@abc.com");
-		Customer c2 = new Customer(1, "Jessica", "pass", "jessica@abc.com");
-		customerList.add(c1);
-		customerList.add(c2);
+	@Autowired
+	CustomersRepository repo;
+	
+
+
+	@GetMapping
+	public Iterable<Customer> getAll() {
+		//  Workshop:  Write an implementation that replies with all customers.
+		//  Your implementation should be no more than a few lines, at most, and make use of the 'repo' object
+		return repo.findAll();   
+	}
+
+	@GetMapping("/{customerId}")
+	public Optional<Customer> getCustomerById(@PathVariable("customerId") long id) {
+		//  Workshop:  Write an implementatoin that looks up one customer.  What do you return if the requested 
+		//  customer ID does not exists?  This implementation could be as short as a single line.
+		return repo.findById(id);
 	}
 	
-	@GetMapping
-	public String getAll() {
-		String response = "[ ";
-		for (int i = 0; i < customerList.size(); i++) {
-			response += customerList.get(i).toJSON();
-			if (i + 1 < customerList.size()) {
-				response += ", ";
-			}
-		}
-		response += " ]";
+	@PostMapping
+	public ResponseEntity<?> addCustomer(@RequestBody Customer newCustomer, UriComponentsBuilder uri) {
+		if (newCustomer.getId()!=0 || newCustomer.getName() == null || newCustomer.getEmail() == null)
+			return ResponseEntity.badRequest().build();
+		
+		newCustomer = repo.save(newCustomer);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newCustomer.getId()).toUri();
+		ResponseEntity<?> response=ResponseEntity.created(location).build();
 		return response;
+
 	}
 
-	public void setListCustomers(ArrayList<Customer> listOfCustomers) {
-		this.customerList = listOfCustomers;
-	}
-	@GetMapping("/{customerId}")
-	public String getCustomerById(@PathVariable("customerId") long id) {
-		String response = "{}";
-		for (int i = 0; i < customerList.size(); i++) {
-			if (customerList.get(i).getId() == id) {
-				response = customerList.get(i).toJSON();
+	//lookupCustomerByName GET
+	@GetMapping("/byname/{username}")
+	public ResponseEntity<?> lookupCustomerByNameGet(@PathVariable("username") String username,
+			UriComponentsBuilder uri) {
+		//  Workshop:  Write an implemenatation to look up a customer by name.  Think about what
+		//  your response should be if no customer matches the name the caller is searching for.
+		//  With the data model implemented in CustomersRepository, do you need to handle more than
+		//  one match per request?
+		ResponseEntity<?> response = null; 
+		if (username == null)
+		{
+			return response;
+		}
+		
+		for (Customer c:repo.findAll())
+		{
+			if (c.getName().equalsIgnoreCase(username) )
+			{
+				
+				URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{username}").buildAndExpand(c.getName()).toUri();
+				response=ResponseEntity.created(location).build();
+				break;
 			}
+
+		}
+		return response;	
+	}
+	
+	//lookupCustomerByName POST
+	@PostMapping("/byname")
+	public ResponseEntity<?> lookupCustomerByNamePost(@RequestBody String username, UriComponentsBuilder uri) {
+		//  Workshop:  Write an implementation to look up a customer by name, using POST semantics
+		//  rather than GET.  You should be able to make use of most of your implmentation for
+		//  lookupCustomerByNameGet().  
+		ResponseEntity<?> response = null; 
+		if (username == null)
+		{
+			return response;
+		}
+		
+		for (Customer c:repo.findAll())
+		{
+			if (c.getName().equalsIgnoreCase(username) )
+			{
+				
+				URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{username}").buildAndExpand(c.getName()).toUri();
+				response=ResponseEntity.created(location).build();
+				break;
+			}
+
 		}
 		return response;
-	}
+	}	
+	
+	
+	@PutMapping("/{customerId}")
+	public ResponseEntity<?> putCustomer(
+			@RequestBody Customer newCustomer,
+			@PathVariable("customerId") long customerId) 
+	{
+		//  Workshop:  Write an implementation to update or create a new customer with an HTTP PUT, with the 
+		//  requestor specifying the customer ID.  Are there error conditions to be handled?  How much data
+		//  validation should you implement considering that customers are stored in a CustomersRepository object.
+		if (newCustomer.getId()!=customerId || newCustomer.getName() == null || newCustomer.getEmail() == null)
+			return ResponseEntity.badRequest().build();
+		
+		newCustomer=repo.save(newCustomer);
+		return ResponseEntity.ok().build();	
+	}	
+	
+	@DeleteMapping("/{customerId}")
+	public ResponseEntity<?> deleteCustomerById(@PathVariable("customerId") long id) {
+		//  Implement a method to delete a customer.  What is an appropriate response? 
+		//
+		//  For discussion (do not worry about implementation):  What are some ways of handling 
+		//  a "delete"?  Is it always the right thing from a business point of view to literally 
+		//  delete a customer entry?  If you did actually delete a customer entry, are there issues
+		//  you could potentially run into later? 
+		if (repo.existsById(id)) {
+			repo.deleteById(id);
+			return ResponseEntity.accepted().build();
+		}
+		return ResponseEntity.badRequest().build();
+	}	
 
 
 	
--- FinalProject/src/main/java/com/ja/msd/mcc/api/EventAPI.java
+++ FinalProject/src/main/java/com/ja/msd/mcc/api/EventAPI.java
@@ -1,25 +1,26 @@
 package com.ja.msd.mcc.api;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import com.ja.msd.mcc.domain.Customer;
 import com.ja.msd.mcc.domain.Event;
+import com.ja.msd.mcc.repository.EventRepository;
 
 public class EventAPI {
 
-		List<Event> eventList = new ArrayList<Event>();
+		@Autowired
+	    EventRepository repo;
 		
-		public EventAPI()
-		{
-			Event e1 = new Event(1L, "Event1", "The Bean", "Free");
-			Event e2 = new Event(2L, "Event2", "Beanzz", "Free");
-			eventList.add(e1);
-			eventList.add(e2);
-		}
 		
 		@GetMapping
 		public List<Event> getAll() {
@@ -44,6 +45,46 @@ public class EventAPI {
 			}
 			return response;
 		}
+		
+		@GetMapping
+		public Iterable<Event> getAll() {
+			//  Workshop:  Implement a method to retrieve all events
+			return null;
+		}
+
+		@GetMapping("/{eventId}")
+		public Optional<Event> getEventById(@PathVariable("eventId") long id) {
+			//  Workshop:  Implement a method to retrieve a single event by it's ID
+			Event response = null;
+			for (int i = 0; i < eventList.size(); i++) {
+				if (eventList.get(i).getId() == id) {
+					response = eventList.get(i);
+				}
+			}
+			return response;
+		}
+		
+		@PostMapping
+		public ResponseEntity<?> addEvent(@RequestBody Event newEvent, UriComponentsBuilder uri) {
+			//  Workshop:  Implement a method to create a new event in response to a POST message.
+			//  Think about how you ensure that the event is properly formed.
+			return null;
+		}
+
+		@PutMapping("/{eventId}")
+		public ResponseEntity<?> putEvent(
+				@RequestBody Event newEvent,
+				@PathVariable("eventId") long eventId) 
+		{
+			//  Workshop:  Implement a method to update an entitye in response to a PUT message.
+			return null;
+		}	
+		
+		@DeleteMapping("/{eventId}")
+		public ResponseEntity<?> deleteEventById(@PathVariable("eventId") long id) {
+			//  Workshop:  Implement a method to delete an entity. 
+			return null;
+		}	
 		
 		
 }
